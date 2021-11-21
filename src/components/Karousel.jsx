6317c0321f14a7482df71be6f405de237f8dbffc
n-rdy <giuseppegelardi@icloud.com>
2021-11-21 16:42:00 -0600

    add detailed information in carouseldata

--- src/components/Karousel.jsx
+++ src/components/Karousel.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Carousel } from 'react-bootstrap/'
-import { Card, CardMedia } from '@material-ui/core'
+import { Card, Container } from '@material-ui/core'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { styled } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
@@ -61,172 +61,238 @@ export default function Karousel() {
       id: 1,
       name: 'Annus Mirabilis',
       image: am1,
-      description: 'describe board 1'
+      description: (`
+      I designed the this board at the peak of Covid-19.\n 
+      It's named Annus Mirabilis (Wonderful Year) in a bit of sarcasm\n 
+      but wanted to pay tribute to the good that did come out of it.\n
+      `),
+      angle: `Angle: 7°`,
+      layout: `Layout: F12 or F13 TKL, WK or WKL`,
+      mounting: `Mounting: Top or O-Ring Gasket`,
+      plate: `Plates: Aluminum, Carbon Fiber or Polycarbonate`,
+      weight: `Stainless Steel or Brass`,
+      colors: `Navy, Silver, Obsidian`
     },
     {
       id: 2,
       name: 'Annus Mirabilis',
       image: am2,
-      description: 'describe board 2'
+      description: (`
+      I designed the this board at the peak of Covid-19.\n 
+      It's named Annus Mirabilis (Wonderful Year) in a bit of sarcasm\n 
+      but wanted to pay tribute to the good that did come out of it.\n
+      `),
+      angle: `Angle: 7°`,
+      layout: `Layout: F12 or F13 TKL, WK or WKL`,
+      mounting: `Mounting: Top or O-Ring Gasket`,
+      plate: `Plates: Aluminum, Carbon Fiber or Polycarbonate`,
+      weight: `Stainless Steel or Brass`,
+      colors: `Navy, Silver, Obsidian`
     },
     {
       id: 3,
       name: 'Annus Mirabilis',
       image: am3,
-      description: 'describe board 3'
+      description: (`
+      I designed the this board at the peak of Covid-19.\n 
+      It's named Annus Mirabilis (Wonderful Year) in a bit of sarcasm\n 
+      but wanted to pay tribute to the good that did come out of it.\n
+      `),
+      angle: `Angle: 7°`,
+      layout: `Layout: F12 or F13 TKL, WK or WKL`,
+      mounting: `Mounting: Top or O-Ring Gasket`,
+      plate: `Plates: Aluminum, Carbon Fiber or Polycarbonate`,
+      weight: `Stainless Steel or Brass`,
+      colors: `Navy, Silver, Obsidian`
     },
     {
       id: 4,
       name: 'Annus Mirabilis',
       image: am4,
-      description: 'describe board 4'
+      description: (`
+      I designed the this board at the peak of Covid-19.\n 
+      It's named Annus Mirabilis (Wonderful Year) in a bit of sarcasm\n 
+      but wanted to pay tribute to the good that did come out of it.\n
+      `),
+      angle: `Angle: 7°`,
+      layout: `Layout: F12 or F13 TKL, WK or WKL`,
+      mounting: `Mounting: Top or O-Ring Gasket`,
+      plate: `Plates: Aluminum, Carbon Fiber or Polycarbonate`,
+      weight: `Stainless Steel or Brass`,
+      colors: `Navy, Silver, Obsidian`
     },
     {
       id: 5,
       name: 'Annus Mirabilis',
       image: am5,
-      description: 'describe board 5'
+      description: (`
+      I designed the this board at the peak of Covid-19.\n 
+      It's named Annus Mirabilis (Wonderful Year) in a bit of sarcasm\n 
+      but wanted to pay tribute to the good that did come out of it.\n
+      `),
+      angle: `Angle: 7°`,
+      layout: `Layout: F12 or F13 TKL, WK or WKL`,
+      mounting: `Mounting: Top or O-Ring Gasket`,
+      plate: `Plates: Aluminum, Carbon Fiber or Polycarbonate`,
+      weight: `Stainless Steel or Brass`,
+      colors: `Navy, Silver, Obsidian`
     },
     {
       id: 6,
       name: 'Annus Mirabilis',
       image: am6,
-      description: 'describe board 6'
+      description: (`
+      I designed the this board at the peak of Covid-19.\n 
+      It's named Annus Mirabilis (Wonderful Year) in a bit of sarcasm\n 
+      but wanted to pay tribute to the good that did come out of it.\n
+      `),
+      angle: `Angle: 7°`,
+      layout: `Layout: F12 or F13 TKL, WK or WKL`,
+      mounting: `Mounting: Top or O-Ring Gasket`,
+      plate: `Plates: Aluminum, Carbon Fiber or Polycarbonate`,
+      weight: `Stainless Steel or Brass`,
+      colors: `Navy, Silver, Obsidian`
     },
     {
       id: 7,
       name: 'Damapad',
       image: bmp1,
-      description: 'describe mp 1'
+      description: (``),
+      angle: ``,
+      layout: ``,
+      mounting: ``,
+      plate: ``,
+      weight: ``,
+      colors: ``
     },
     {
       id: 8,
       name: 'Damapad',
       image: bmp2,
-      description: 'describe mp 2'
+      description: ``
     },
     {
       id: 9,
       name: 'BolsAlice',
       image: bolsalice2,
-      description: 'describe mp 6'
+      description: ``
     },
     {
       id: 10,
       name: 'BolsAlice',
       image: bolsalice1,
-      description: 'describe mp 6'
+      description: ``
     },
     {
       id: 11,
       name: 'BolsAlice',
       image: bolsalice3,
-      description: 'describe mp 6'
+      description: ``
     },
     {
       id: 12,
       name: 'GGB',
       image: ggb1,
-      description: 'describe ggb 1'
+      description: ``
     },
     {
       id: 13,
       name: 'GGB',
       image: ggb2,
-      description: 'describe mp 2'
+      description: ``
     },
     {
       id: 14,
       name: 'Midway60',
       image: midway1,
-      description: 'describe midway 1'
+      description: ``
     },
     {
       id: 15,
       name: 'Midway60',
       image: midway2,
-      description: 'describe midway 2'
+      description: ``
     },
     {
       id: 16,
       name: 'MoonDrop',
       image: nathan1,
-      description: 'describe moondrop 1'
+      description: ``
     },
     {
       id: 17,
       name: 'MoonDrop',
       image: nathan2,
-      description: 'describe moondrop 2'
+      description: ``
     },
     {
       id: 18,
       name: 'MoonDrop',
       image: nathan3,
-      description: 'describe moondrop 3'
+      description: ``
     },
     {
       id: 19,
       name: 'MoonDrop',
       image: nathan4,
-      description: 'describe moondrop 4'
+      description: ``
     },
     {
       id: 20,
       name: 'MoonDrop',
       image: nathan5,
-      description: 'describe moondrop 5'
+      description: ``
     },
     {
       id: 21,
       name: 'MoonDrop',
       image: nathan6,
-      description: 'describe moondrop 6'
+      description: ``
     },
     {
       id: 22,
       name: 'PrimeElise',
       image: pe1,
-      description: 'describe pe1 1'
+      description: ``
     },
     {
       id: 23,
       name: 'PrimeElise',
       image: pe2,
-      description: 'describe pe1 2'
+      description: ``
     },
     {
       id: 24,
       name: 'PrimeElise',
       image: pe3,
-      description: 'describe pe1 3'
+      description: ``
     },
     {
       id: 25,
       name: 'PrimeElise',
       image: pe4,
-      description: 'describe pe1 4'
+      description: ``
     },
     {
       id: 26,
       name: 'Alice',
       image: persalice1,
-      description: 'describe persalice1'
+      description: ``
     },
     {
       id: 27,
       name: 'Alice',
       image: persalice2,
-      description: 'describe persalice2'
+      description: ``
     }
   ]
 
   return (
-    <div style={{marginTop: '20px'}}>
-      <Card>
-        <CardMedia>
-          <Carousel interval={25000} m='5' p='md'>
-
+    <Container style={{marginTop: '20px'}} elevation={20} >
+    {/* <div style={{marginTop: '20px'}}> */}
+      <Card elevation={20}>
+        {/* <CardMedia> */}
+          <Carousel interval={25000} >
             {carouselItems.map((item) => (
               <Carousel.Item 
                 key={item.id}>
@@ -235,9 +301,9 @@ export default function Karousel() {
                     src={item.image}
                     alt=''
                   />
-                  
-                  <Card style={{textAlign: 'center', marginTop: '50px', marginBottom: '50px'}}>
-                  <Typography>
+<Container maxWidth='xs'>
+                <Card style={{textAlign: 'center', marginTop: '50px', marginBottom: '50px'}}>
+                  <Typography style={{marginTop: '10px'}}>
                     <h3>{item.name}</h3>
                   </Typography>
                   Learn More
@@ -251,17 +317,27 @@ export default function Karousel() {
                   </ExpandMore>
 
                   <Collapse in={expanded} timeout="auto" unmountOnExit>
-                    <CardContent>
+                    {/* <Card mx='50'> */}
+                    <CardContent >
                       <Typography paragraph>{item.description}</Typography>
+                      <Typography paragraph>{item.angle}</Typography>
+                      <Typography paragraph>{item.layout}</Typography>
+                      <Typography paragraph>{item.mounting}</Typography>
+                      <Typography paragraph>{item.plate}</Typography>
+                      <Typography paragraph>{item.weight}</Typography>
+                      <Typography paragraph>{item.colors}</Typography>
                     </CardContent>
+                    {/* </Card> */}
                   </Collapse>
 
                 </Card>
+    </Container>
               </Carousel.Item>
             ))}
           </Carousel>
-        </CardMedia>
+        {/* </CardMedia> */}
       </Card>
-    </div>
+    {/* </div> */}
+    </Container>
   )
 }
