3c6bdc9e5410fee0d8f94caddc7047b83bfdbe1d
n-rdy <giuseppegelardi@icloud.com>
2021-11-21 13:45:42 -0600

    change page title, add more img

--- src/components/Karousel.jsx
+++ src/components/Karousel.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap/'
 import { Card } from '@material-ui/core'
-import './styles/Karousel.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -11,6 +10,21 @@ import am3 from '../img/am3.png'
 import am4 from '../img/am4.png'
 import am5 from '../img/am5.png'
 import am6 from '../img/am6.png'
+import bmp1 from '../img/bmp1.png'
+import bmp2 from '../img/bmp2.png'
+import bolsalice1 from '../img/bosalice1.png'
+import bolsalice2 from '../img/bosalice2.png'
+import bolsalice3 from '../img/bolsalice3.png'
+import ggb1 from '../img/ggb1.PNG'
+import ggb2 from '../img/ggb2.PNG'
+import midway1 from '../img/midway1.jpg'
+import midway2 from '../img/midway2.png'
+import nathan1 from '../img/nathan1.png'
+import nathan2 from '../img/nathan2.png'
+import nathan3 from '../img/nathan3.png'
+import nathan4 from '../img/nathan4.png'
+import nathan5 from '../img/nathan5.png'
+import nathan6 from '../img/nathan6.png'
 
 
 export default function Karousel() {
@@ -52,6 +66,96 @@ export default function Karousel() {
       image: am6,
       description: 'describe board 6'
     },
+    {
+      id: 7,
+      name: 'Damapad',
+      image: bmp1,
+      description: 'describe mp 1'
+    },
+    {
+      id: 8,
+      name: 'Damapad',
+      image: bmp2,
+      description: 'describe mp 2'
+    },
+    {
+      id: 9,
+      name: 'BolsAlice',
+      image: bolsalice2,
+      description: 'describe mp 6'
+    },
+    {
+      id: 10,
+      name: 'BolsAlice',
+      image: bolsalice1,
+      description: 'describe mp 6'
+    },
+    {
+      id: 11,
+      name: 'BolsAlice',
+      image: bolsalice3,
+      description: 'describe mp 6'
+    },
+    {
+      id: 12,
+      name: 'GGB',
+      image: ggb1,
+      description: 'describe ggb 1'
+    },
+    {
+      id: 13,
+      name: 'GGB',
+      image: ggb2,
+      description: 'describe mp 2'
+    },
+    {
+      id: 14,
+      name: 'Midway60',
+      image: midway1,
+      description: 'describe midway 1'
+    },
+    {
+      id: 15,
+      name: 'Midway60',
+      image: midway2,
+      description: 'describe midway 2'
+    },
+    {
+      id: 16,
+      name: 'MoonDrop',
+      image: nathan1,
+      description: 'describe moondrop 1'
+    },
+    {
+      id: 17,
+      name: 'MoonDrop',
+      image: nathan2,
+      description: 'describe moondrop 2'
+    },
+    {
+      id: 18,
+      name: 'MoonDrop',
+      image: nathan3,
+      description: 'describe moondrop 3'
+    },
+    {
+      id: 19,
+      name: 'MoonDrop',
+      image: nathan4,
+      description: 'describe moondrop 4'
+    },
+    {
+      id: 20,
+      name: 'MoonDrop',
+      image: nathan5,
+      description: 'describe moondrop 5'
+    },
+    {
+      id: 21,
+      name: 'MoonDrop',
+      image: nathan6,
+      description: 'describe moondrop 6'
+    },
   ]
 
   return (
@@ -62,7 +166,6 @@ export default function Karousel() {
 
         {carouselItems.map((item) => (
           <Carousel.Item 
-            // style={{margin: 'auto'}}
             key={item.id}>
               <img 
                 className='d-dblock w-100'
